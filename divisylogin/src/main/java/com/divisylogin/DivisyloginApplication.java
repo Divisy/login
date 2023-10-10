93d7cdc98920bcf62d6ed52f95762167246aa67e
Divisy <125465793+Divisy@users.noreply.github.com>
2023-10-10 08:05:39 +0200

    starting
    Commit with first files

---
+++ divisylogin/src/main/java/com/divisylogin/DivisyloginApplication.java
@@ -0,0 +1,13 @@
+package com.divisylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivisyloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivisyloginApplication.class, args);
+	}
+
+}
---
+++ divisylogin/src/test/java/com/divisylogin/DivisyloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divisylogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivisyloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
