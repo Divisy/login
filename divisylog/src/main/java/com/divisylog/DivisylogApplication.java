9e03938a64b714c0d371ff1044c27d9325a944f5
Divisy <125465793+Divisy@users.noreply.github.com>
2023-10-10 08:29:10 +0200

    Start configuration

---
+++ divisylog/src/main/java/com/divisylog/DivisylogApplication.java
@@ -0,0 +1,13 @@
+package com.divisylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivisylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivisylogApplication.class, args);
+	}
+
+}
---
+++ divisylog/src/test/java/com/divisylog/DivisylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divisylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivisylogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
